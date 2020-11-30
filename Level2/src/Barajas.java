25bda4f92c4383007ce4afa582d27e7f19cd2d28
Franco adrian <adriantonanes@gmail.com>
2020-11-29 21:22:14 -0300

    Todas las cartas

--- Level2/src/Barajas.java
+++ Level2/src/Barajas.java
@@ -3,11 +3,15 @@ import java.util.Collections;
 
 public class Barajas {
     public static void main(String[] args) {
-        ArrayList<Integer> cartas = new ArrayList<>();
-
+        ArrayList<String> cartas = new ArrayList<>();
+        cartas.add("A");
         for(int i = 1; i <= 10; i++){
-            cartas.add(i);
+            String a = String.valueOf(i);
+            cartas.add(a);
         }
+        cartas.add("J");
+        cartas.add("Q");
+        cartas.add("k");
         System.out.println(cartas);
         Collections.reverse(cartas);
         System.out.println(cartas);
