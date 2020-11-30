6ba3fa110e211e9692f42e4bacf78d5279241b0f
Franco adrian <adriantonanes@gmail.com>
2020-11-29 21:25:13 -0300

    Correccion

--- Level2/src/Barajas.java
+++ Level2/src/Barajas.java
@@ -4,11 +4,12 @@ import java.util.Collections;
 public class Barajas {
     public static void main(String[] args) {
         ArrayList<String> cartas = new ArrayList<>();
-        cartas.add("A");
+
         for(int i = 1; i <= 10; i++){
             String a = String.valueOf(i);
             cartas.add(a);
         }
+        cartas.add(0, "A");
         cartas.add("J");
         cartas.add("Q");
         cartas.add("k");
